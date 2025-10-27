081677f0648c3b6abeea8ce29379e77568b69fc5
immanelg <119798691+immanelg@users.noreply.github.com>
2025-10-27 22:18:47 +0300

    slop

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import sys
 import re
 import pathlib
+import time
 import logging
 from telethon import TelegramClient
 from telethon.utils import is_audio, is_gif, is_image, is_video
@@ -30,7 +31,7 @@ def usage():
     eprint("    -L: list of all chats")
     eprint("    -LL: list of links of subscribed channels")
     eprint("    -S: restore a list of channels from links")
-    eprint("    -C id: export saved messages")
+    eprint("    -C id: export saved messages to ./${phonenumber}/chat-messages.md")
     eprint()
     eprint("GLOBAL OPTIONS:")
     eprint("    --help: print this help message")
@@ -42,15 +43,17 @@ password: str = os.environ.get("TELEGRAM_PASSWORD") or ""
 api_id: int = int(os.environ.get("TELEGRAM_API_ID") or usage())
 api_hash: str = os.environ.get("TELEGRAM_API_HASH") or usage()
 
-logging.basicConfig(stream=sys.stderr, level=logging.INFO, format="[%(asctime)s] %(levelname)s - %(message)s")
+logging.basicConfig(stream=sys.stderr, level=logging.INFO, format="[%(asctime)s] %(levelname)s %(module)s - %(message)s")
 
+session_dir = pathlib.Path(phone)
+session_dir.mkdir(exist_ok=True, parents=True)
 
 async def main():
-    client = TelegramClient(session=phone, api_id=api_id, api_hash=api_hash)
+    client = TelegramClient(session=session_dir/"session", api_id=api_id, api_hash=api_hash)
 
     await client.start(phone=phone, password=password, force_sms=False)
 
-    # Here comes the deepest indentation I ever wrote in python
+    # Here comes the deepest indentation I ever wrote in python (and also the sloppiest code in existence)
     match sys.argv[1:]:
         case ["-L"]:
             dialogs = await client.get_dialogs()
@@ -67,159 +70,167 @@ async def main():
 
             for link in sys.stdin:
                 await client(JoinChannelRequest(link))
-        case ["-C", id]:
-            logging.info(f"Saving chat: {id}")
 
-            chat = await client.get_input_entity(int(id))
-            n = 0
-            last_group_id = None
-            async for message in client.iter_messages(chat, reverse=True, limit=None):
-                dump = 0
-                if dump:
-                    print("Id:", message.id)
-                    print("GroupId:", message.grouped_id)  # album
-                    print("Date:", message.date)
-                    print("Entities:", message.entities)
-                    print("Message:", message.message)
-                    print("Poll:", message.poll)
-                    print("File!:", message.file)
-                    if f := message.file:
-                        print("audio video gif image -- ", is_audio(f), is_video(f), is_gif(f), is_image(f))
-                        print("file.name:", f.name)
-                        # print(dir(f))
-                        for attr in (
-                            "duration",
-                            "emoji",
-                            "ext",
-                            "height",
-                            "id",
-                            "media",
-                            "mime_type",
-                            "name",
-                            "performer",
-                            "size",
-                            "sticker_set",
-                            "title",
-                            "width",
-                        ):
-                            if hasattr(f, attr):
-                                if v := getattr(f, attr):
-                                    print(attr, v)
-                    print("Forward:", message.forward)
-                    if fw := message.forward:
-                        chat = fw.chat
-                        print("fwd", chat.title, chat.id)
-                    print("Reactions:", message.reactions)
-                    # print("Media:", message.media)
-                    # print("Photo:", message.photo)
-                    # print("Video:", message.video)
-                    # print("Voice:", message.voice)
-                    # print("Audio:", message.audio)
-                    # print("Sticker:", message.sticker)
-                    # print("Doc:", message.document)
-                    print("---------------------------------------------")
-                else:
-                    logging.info(f"Message {message.id}...")
-                    continuing_group = message.grouped_id is not None and last_group_id == message.grouped_id
-                    last_group_id = message.grouped_id
+        case ["-C", chatid]:
+            logging.info(f"Saving chat: {chatid}")
 
-                    if continuing_group:
-                        # print("(grouped)")
-                        pass
-                    if not continuing_group:
-                        # finish the previous one
-                        # print("---")
-                        pass
+            data_path = session_dir/chatid
+            data_path.mkdir(parents=True, exist_ok=True)
+            messages_md_path = data_path/"chat-messages.md"
 
-                    if not continuing_group:
-                        # Create message prologue.
-
-                        print(f"<h3 id='m_{message.id}'>Message m_{message.id}</h3>")
-                        print()
-                        print(message.date.strftime("Date: %Y-%m-%d_%H:%M:%S"))
-                        print()
-
+            logging.info(f"Opening file {messages_md_path} for writing chat archive")
+            with open(messages_md_path, "w", encoding="utf-8") as out:
+                start_time = time.perf_counter()
+                chat = await client.get_input_entity(int(chatid))
+                n = 0
+                last_group_id = None
+                async for message in client.iter_messages(chat, reverse=True, limit=None):
+                    dump = 0
+                    if dump:
+                        out.write(f"Id: {message.id}\n")
+                        out.write(f"GroupId: {message.grouped_id}\n")  # album
+                        out.write(f"Date: {message.date}\n")
+                        out.write(f"Entities: {message.entities}\n")
+                        out.write(f"Message: {message.message}\n")
+                        out.write(f"Poll: {message.poll}\n")
+                        out.write(f"File!: {message.file}\n")
+                        if f := message.file:
+                            out.write(f"audio video gif image -- {is_audio(f)} {is_video(f)} {is_gif(f)} {is_image(f)}\n")
+                            out.write(f"file.name: {f.name}\n")
+                            # out.write(dir(f))
+                            for attr in (
+                                "duration",
+                                "emoji",
+                                "ext",
+                                "height",
+                                "id",
+                                "media",
+                                "mime_type",
+                                "name",
+                                "performer",
+                                "size",
+                                "sticker_set",
+                                "title",
+                                "width",
+                            ):
+                                if hasattr(f, attr):
+                                    if v := getattr(f, attr):
+                                        out.write(f"{attr} {v}\n")
+                        out.write(f"Forward: {message.forward}\n")
                         if fw := message.forward:
                             chat = fw.chat
-                            print("Forwarded from:", chat.title)
-                            print()
+                            out.write(f"fwd {chat.title} {chat.id}\n")
+                        out.write(f"Reactions: {message.reactions}\n")
+                        # out.write("Media:", message.media)
+                        # out.write("Photo:", message.photo)
+                        # out.write("Video:", message.video)
+                        # out.write("Voice:", message.voice)
+                        # out.write("Audio:", message.audio)
+                        # out.write("Sticker:", message.sticker)
+                        # out.write("Doc:", message.document)
+                        out.write("---------------------------------------------\n")
+                    else:
+                        logging.info(f"Message {message.id}...")
+                        continuing_group = message.grouped_id is not None and last_group_id == message.grouped_id
+                        last_group_id = message.grouped_id
 
-                        if rpl := message.reply_to:
-                            hsh = f"#m_{rpl.reply_to_msg_id}"
-                            print(f"Replied to: [{hsh}]({hsh})")
-                            if q := rpl.quote_text:
-                                for line in q.split("\n"):
-                                    print(f"> {line}")
-                            print()
+                        if continuing_group:
+                            # out.write("(grouped)")
+                            pass
+                        if not continuing_group:
+                            # finish the previous one
+                            # out.write("---")
+                            pass
 
-                        if p := message.poll:
-                            # POLL: message.poll =  MessageMediaPoll(poll=Poll(id=5472212957045724922, question=TextWithEntities(text='poll', entities=[]), answers=[PollAnswer(text=TextWithEntities(text='1', entities=[]), option=b'0'), PollAnswer(text=TextWithEntities(text='2', entities=[]), option=b'1')], closed=False, public_voters=True, multiple_choice=False, quiz=True, close_period=None, close_date=None), results=PollResults(min=False, results=[PollAnswerVoters(option=b'0', voters=0, chosen=False, correct=False), PollAnswerVoters(option=b'1', voters=1, chosen=True, correct=True)], total_voters=1, recent_voters=[PeerUser(user_id=8297349295)], solution='EXPLANATION!', solution_entities=[]))
-                            print("Poll:", p.poll.question.text)
-                            for answer in p.poll.answers:
-                                print("  * ", answer.text.text)
-                            print()
-                            if p.results.solution:
-                                print("Explanation:", p.results.solution)
+                        if not continuing_group:
+                            # Create message prologue.
 
-                        if txt := message.message:
-                            if entities := message.entities:
-                                entities = sorted(entities, key=lambda x: x.offset, reverse=True)
-                                for entity in entities:
-                                    start = entity.offset
-                                    end = entity.offset + entity.length
-                                    part = txt[start:end]
+                            out.write(f"<h3 id='m_{message.id}'>Message m_{message.id}</h3>\n")
+                            out.write("\n")
+                            out.write(f"{message.date.strftime('Date: %Y-%m-%d_%H:%M:%S')}\n")
+                            out.write("\n")
 
-                                    if isinstance(entity, MessageEntityBold):
-                                        part_md = f"**{part}**"
-                                    elif isinstance(entity, MessageEntityItalic):
-                                        part_md = f"*{part}*"
-                                    elif isinstance(entity, MessageEntityCode):
-                                        part_md = f"`{part}`"
-                                    elif isinstance(entity, MessageEntityPre):
-                                        part_md = f"```{part}```"
-                                    elif isinstance(entity, MessageEntityTextUrl):
-                                        part_md = f"[{part}]({entity.url})"
-                                    elif isinstance(entity, MessageEntityStrike):
-                                        part_md = f"~~{part}~~"
-                                    else:
-                                        continue
+                            if fw := message.forward:
+                                chat = fw.chat
+                                out.write(f"Forwarded from: {chat.title}\n")
+                                out.write("\n")
 
-                                    txt = txt[:start] + part_md + txt[end:]
-                            print(txt)
-                            print()
+                            if rpl := message.reply_to:
+                                hsh = f"#m_{rpl.reply_to_msg_id}"
+                                out.write(f"Replied to: [{hsh}]({hsh})\n")
+                                if q := rpl.quote_text:
+                                    for line in q.split("\n"):
+                                        out.write(f"> {line}\n")
+                                out.write("\n")
 
-                    # In group, we are only interested in files.
-                    if f := message.file:
-                        dl_dir = pathlib.Path("./tgfiles")
-                        dl_dir.mkdir(exist_ok=True)
+                            if p := message.poll:
+                                # POLL: message.poll =  MessageMediaPoll(poll=Poll(id=5472212957045724922, question=TextWithEntities(text='poll', entities=[]), answers=[PollAnswer(text=TextWithEntities(text='1', entities=[]), option=b'0'), PollAnswer(text=TextWithEntities(text='2', entities=[]), option=b'1')], closed=False, public_voters=True, multiple_choice=False, quiz=True, close_period=None, close_date=None), results=PollResults(min=False, results=[PollAnswerVoters(option=b'0', voters=0, chosen=False, correct=False), PollAnswerVoters(option=b'1', voters=1, chosen=True, correct=True)], total_voters=1, recent_voters=[PeerUser(user_id=8297349295)], solution='EXPLANATION!', solution_entities=[]))
+                                out.write(f"Poll: {p.poll.question.text}\n")
+                                for answer in p.poll.answers:
+                                    out.write(f"  * {answer.text.text}\n")
+                                out.write("\n")
+                                if p.results.solution:
+                                    out.write(f"Explanation: {p.results.solution}\n")
 
-                        escape = lambda text: re.sub(r"[^A-Za-z0-9.]+", "", text)
+                            if txt := message.message:
+                                if entities := message.entities:
+                                    entities = sorted(entities, key=lambda x: x.offset, reverse=True)
+                                    for entity in entities:
+                                        start = entity.offset
+                                        end = entity.offset + entity.length
+                                        part = txt[start:end]
 
-                        # name = f"{message.id}_{message.date.strftime('%Y-%m-%d_%H-%M-%S')}"
-                        name = f"{message.id}"
-                        if f.name:
-                            name += "-" + escape(f.name)
-                        elif f.ext:
-                            name += f.ext
-                        path = dl_dir / name
+                                        if isinstance(entity, MessageEntityBold):
+                                            part_md = f"**{part}**"
+                                        elif isinstance(entity, MessageEntityItalic):
+                                            part_md = f"*{part}*"
+                                        elif isinstance(entity, MessageEntityCode):
+                                            part_md = f"`{part}`"
+                                        elif isinstance(entity, MessageEntityPre):
+                                            part_md = f"```{part}```"
+                                        elif isinstance(entity, MessageEntityTextUrl):
+                                            part_md = f"[{part}]({entity.url})"
+                                        elif isinstance(entity, MessageEntityStrike):
+                                            part_md = f"~~{part}~~"
+                                        else:
+                                            continue
 
-                        if not path.exists():
-                            logging.info(f"Downloading media: {path}")
-                            await message.download_media(file=path)
-                        else:
-                            logging.info(f"Media cache hit: {path}")
+                                        txt = txt[:start] + part_md + txt[end:]
+                                out.write(f"{txt}\n")
+                                out.write("\n")
 
-                        print(f"File: [{name}]({path}), {f.mime_type} ({int(f.size / 1024)} kb)")
-                        print()
+                        # In group, we are only interested in files.
+                        if f := message.file:
+                            dl_dir = data_path/"tgfiles"
+                            dl_dir.mkdir(exist_ok=True, parents=True)
 
-                        print()
+                            escape = lambda text: re.sub(r"[^A-Za-z0-9.]+", "", text)
 
-                n += 1
-                if n > 128:
-                    break
+                            # name = f"{message.id}_{message.date.strftime('%Y-%m-%d_%H-%M-%S')}"
+                            name = f"{message.id}"
+                            if f.name:
+                                name += "-" + escape(f.name)
+                            elif f.ext:
+                                name += f.ext
+                            path = dl_dir / name
 
-            logging.info(f"Done. Total: {n}")
-            print("\n\n\n------------------------------\n\n\nTOTAL:", n)
+                            if not path.exists():
+                                logging.info(f"Downloading media: {path}")
+                                await message.download_media(file=path)
+                            else:
+                                logging.info(f"Media cache hit: {path}")
+
+                            out.write(f"File: [{name}](tgfiles/{name}), {f.mime_type} ({int(f.size / 1024)} kb)\n")
+                            out.write("\n")
+
+
+                    n += 1
+                    if n > 128:
+                        break
+
+                end_time = time.perf_counter() 
+                logging.info(f"Done. Total: {n}, took {end_time - start_time:.4f}s.")
+                out.write(f"\n\n\n------------------------------\n\n\nTOTAL: {n}\n")
         case _:
             usage()
 
